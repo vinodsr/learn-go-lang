81a6be0c3a20290b58ac8920488e2573c67b0bab
Vinod S R <mailme@vinodsr.com>
2016-09-03 22:51:10 +0530

    added comments

--- src/main/array.go
+++ src/main/array.go
@@ -9,7 +9,7 @@ func main() {
 
 	list := [4]int{10, 20, 30, 40}
 	fmt.Println(list)
-
+	/// create a  array with max of 10 elements
 	scores := make([]int, 0, 10)
 	//scores[7] = 9033
 	scores = append(scores, 5)
@@ -19,4 +19,11 @@ func main() {
 	fmt.Println(scores)
 	c := cap(scores)
 	fmt.Print(c)
+	test()
+	likes := make([]int, 10, 10)
+	fmt.Println("likes : ", likes)
+}
+
+func test() {
+
 }
