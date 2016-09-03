da3fd5f19a84d1e1e722fb058751de7b8ec07558
Vinod S R <mailme@vinodsr.com>
2016-09-03 22:43:31 +0530

    added array program

---
+++ src/main/array.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	var data [4]int
+	data[0] = 100
+	fmt.Println(data)
+
+	list := [4]int{10, 20, 30, 40}
+	fmt.Println(list)
+
+	scores := make([]int, 0, 10)
+	//scores[7] = 9033
+	scores = append(scores, 5)
+	scores = append(scores, 15)
+	scores = scores[0:8]
+	scores[7] = 9033
+	fmt.Println(scores)
+	c := cap(scores)
+	fmt.Print(c)
+}
